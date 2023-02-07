efd1248ba07d39bb486f0c28290af96ecc0e3ec1
rumeone <yuaroslav_29@mail.ru>
2023-02-08 01:51:45 +0700

    finished work with sequelize config

--- server/todo/src/app.module.ts
+++ server/todo/src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common'
 import {TodoModule} from "./todos/todo.module";
+import {SequelizeModule} from "@nestjs/sequelize";
+import {ConfigModule} from "@nestjs/config";
+import {SequelizeConfigService} from "./config/sequelize.config.service";
+import {databaseConfig} from "./config/configuration";
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+      TodoModule,
+      SequelizeModule.forRootAsync({
+        imports: [ConfigModule],
+        useClass: SequelizeConfigService
+      }),
+      ConfigModule.forRoot({
+          load: [databaseConfig]
+      }),
+      TodoModule
+  ],
 })
 export class AppModule {}
